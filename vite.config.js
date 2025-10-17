d550eb40d8959ba5f774aba1eb93751f6340b472
Vicente <vi.bueno@duocuc.cl>
2025-10-17 11:35:43 -0300

    Correcciones y preparación para despliegue en GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// 👇 usa el nombre EXACTO de tu repositorio en GitHub
 export default defineConfig({
   plugins: [react()],
+  base: '/AnimeKai/', // <--- importante: el nombre del repo
 })
+
